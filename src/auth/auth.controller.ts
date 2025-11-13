1a17874afb46e8221d1576afa0721e07a77b6eb7
chakibbelho <chakibbelhocine@gmail.com>
2025-11-13 11:52:12 +0100

    auth logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post ,Req} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import type { Request } from 'express';
+import type { Authdto } from './dto';
 
 
 @Controller('auth')
@@ -8,8 +8,13 @@ export class AuthController {
     constructor(private authService: AuthService) {
     }
 @Post('signup')
-    signUp(@Req() req: Request) {{
-        console.log(req);
+    signUp(@Body('email') email: string, @Body('password') password: string ) {{
+        console.log({
+        email,
+        typeOfEmail: typeof email,
+        password,
+        typeOfPassword: typeof password,
+        });
 return this.authService.signUp();
     }}
 
---
+++ src/auth/dto/auth.dto.ts
@@ -0,0 +1,5 @@
+export class Authdto{
+    email: string;
+    password: string;
+
+}
